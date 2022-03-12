7f97cdbec94faf204d6a6f6772b5d9fe7b48207a
pjt3591oo <pjt3591oo@gmail.com>
2022-03-12 11:20:54 +0900

    datalab 임시 코드 생성

--- naver_ad.py
+++ naver_ad.py
@@ -1,14 +1,17 @@
-import requests as rq
-import time, os, hashlib, hmac, base64
+import time, os, hashlib, hmac, base64, urllib3, json
+from bs4 import BeautifulSoup
 
-import urllib3
+import requests as rq
+
+
 urllib3.disable_warnings()
 
-CUSTOMER_ID = os.environ['CUSTOMER_ID']
-API_KEY = os.environ['API_KEY']
-SECRET_KEY = os.environ['SECRET_KEY']
+CUSTOMER_ID = os.environ.get('CUSTOMER_ID', '')
+API_KEY = os.environ.get('API_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-BASE_URL = "https://api.naver.com"
+if CUSTOMER_ID or API_KEY or SECRET_KEY:
+  print('[ERROR] naver api키 설정이 되있지 않습니다.')
 
 def _generate(timestamp, method, path, secret_key):
   message = f"{timestamp}.{method}.{path}"
@@ -34,10 +37,12 @@ def _get_header(method, path):
     'X-Signature': signature
   }
 
+# NAVER SEARCH AD API
 def search(keyword):
-  
+  BASE_URL = "https://api.naver.com"
   path = '/keywordstool'
   headers = _get_header('GET', path)
+
   res = rq.get(
     f"{BASE_URL}{path}", 
     headers=headers, 
@@ -48,9 +53,39 @@ def search(keyword):
   )
 
   print(f'[API 결과] HTTP 응답코드: {res.status_code}, API: 키워드 검색: {keyword}') 
+  print(res.json())
   data = res.json()['keywordList'][0]
   
   return data
 
+# NANER DATALAB 
+
+def dataLabSearch(keyword):
+  BASE_URL = 'https://datalab.naver.com'
+  path = '/qcHash.naver'
+  headers = {
+    'referer': 'https://datalab.naver.com/keyword/trendResult.naver',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'PostmanRuntime/7.29.0'
+  }
+  payload = {
+    "queryGroups" : '파이썬__SZLIG__파이썬',
+    "startDate" : '20220301',
+    "endDate" : '20220312',
+    "timeUnit" : 'date',
+    'device': 'pc'
+  }
+  res = rq.post(f'{BASE_URL}{path}', headers=headers, data=payload)
+  hashKey = res.json().get('hashKey')
+  print(hashKey)
+  url = f'https://datalab.naver.com/keyword/trendResult.naver?hashKey={hashKey}'
+  print(url)
+  res = rq.get(url, headers=headers)
+  soup = BeautifulSoup(res.content, 'html.parser')
+  data = soup.find(id='graph_data').text
+  d = json.loads(data)
+  return d
+
 if __name__ == "__main__":
-  print(search("python"))
+  # print(search("python"))
+  print(dataLabSearch('LFMALL'))
